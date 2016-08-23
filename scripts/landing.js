58b970e009e0928ac0dab40ef887b32f8f150c09
Ashwin Khurana <ashwin@ashwinkhurana.com>
2016-08-22 20:06:55 -0700

    assignment-9-events: completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,21 +1,17 @@
 var pointsArray = document.getElementsByClassName('point');
  
+var revealPoint = function(point) {
+     point.style.opacity = 1;
+     point.style.transform = "scaleX(1) translateY(0)";
+     point.style.msTransform = "scaleX(1) translateY(0)";
+     point.style.WebkitTransform = "scaleX(1) translateY(0)";
+};
+
 var animatePoints = function(points) {
  
+     forEach(points, revealPoint);
      var points = document.getElementsByClassName("point");
- 
-     var revealPoint = function() {
-         
-         for (var i = 0; i < points.length; i++) {
-             points[i].style.opacity = 1;
-             points[i].style.transform = "scaleX(1) translateY(0)";
-             points[i].style.msTransform = "scaleX(1) translateY(0)";
-             points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-             
-         };
-     };
-    
-     revealPoint(); 
+  
  };
 
 window.onload = function() {
