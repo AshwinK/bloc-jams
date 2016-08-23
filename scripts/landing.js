3c27705c7e35952afdb082cb47e3825d0f141ce1
Ashwin Khurana <ashwin@ashwinkhurana.com>
2016-08-22 18:52:48 -0700

    checkpoint-9-events: completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -25,6 +25,7 @@ window.onload = function() {
     }
     
     var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    
     var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
     
     window.addEventListener('scroll', function(event) {
